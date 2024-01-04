938b26aeee86725e9da39ef1ae2441e0dd940429
S_RankHero <152952078+jinko0404@users.noreply.github.com>
2024-01-04 17:32:55 +0900

    [ADD]객체, 클래스, 메소드 개념 예제
    모텔 관리 시스템 실습 코드

---
+++ src/org/Super/day05/Exam_Access.java
@@ -0,0 +1,16 @@
+package org.Super.day05;
+
+class Sample{
+	public int a;
+	private int b;
+	int c;
+}
+
+public class Exam_Access {
+
+	public static void main(String[] args) {
+		Sample sam = new Sample();
+
+	}
+
+}
